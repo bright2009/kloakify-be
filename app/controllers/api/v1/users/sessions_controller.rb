0717ca1bf0f1186af64bf9cfa8d33fd07a02213c
Ukaypromise <promiseukaimo@gmail.com>
2024-01-23 16:32:26 +0100

    Implement rack-session fix

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,28 +1,42 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
+  include RackSessionFix
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opts = {})
+    render json: {
+             status: { code: 200, message: "Logged in sucessfully." },
+             data: UserSerializers.new(current_user).serializable_hash,
+           }, status: :ok
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def respond_to_on_destroy
+    authorization_header = request.headers["Authorization"]
 
-  # protected
+    if authorization_header.present?
+      jwt_token = authorization_header.split(" ")[1]
+      jwt_payload = JWT.decode(jwt_token, Rails.application.credentials.fetch(:secret_key_base)).first
+      current_user = User.find(jwt_payload["sub"])
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+      if current_user
+        render json: {
+                 status: 200,
+                 message: "logged out successfully",
+               }, status: :ok
+      else
+        render json: {
+                 status: 401,
+                 message: "Couldn't find an active session.",
+               }, status: :unauthorized
+      end
+    else
+      render json: {
+               status: 401,
+               message: "Authorization header missing.",
+             }, status: :unauthorized
+    end
+  end
 end
