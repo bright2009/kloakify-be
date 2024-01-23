d0b3aefeb3e811fa0b1aa5c11fe1161fa4e1061b
Ukaypromise <promiseukaimo@gmail.com>
2024-01-23 16:21:31 +0100

    Implement devise-jwt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,6 +264,16 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   config.navigational_formats = []
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ["POST", %r{^/sign_in$}],
+    ]
+    jwt.revocation_requests = [
+      ["DELETE", %r{^/sign_out$}],
+    ]
+    jwt.expiration_time = 2.days.to_i
+  end
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
